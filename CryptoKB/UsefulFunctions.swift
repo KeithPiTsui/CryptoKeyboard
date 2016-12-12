a6827ff5c8b00ff48824c9cfe6671d00756c643d
KeithPiTsui <85331524@qq.com>
2016-12-12 12:57:15 +0800

    add some useful functions

---
+++ CryptoKB/UsefulFunctions.swift
@@ -0,0 +1,60 @@
+//
+//  UsefulFunction.swift
+//  CryptoKeyboard
+//
+//  Created by Pi on 12/12/2016.
+//  Copyright © 2016 Keith. All rights reserved.
+//
+
+import UIKit
+
+func delay(_ delay:Double, closure:@escaping ()->()) {
+    let when = DispatchTime.now() + delay
+    DispatchQueue.main.asyncAfter(deadline: when, execute: closure)
+}
+
+extension CGRect {
+    init(_ x:CGFloat, _ y:CGFloat, _ w:CGFloat, _ h:CGFloat) {
+        self.init(x:x, y:y, width:w, height:h)
+    }
+    var center : CGPoint {
+        return CGPoint(self.midX, self.midY)
+    }
+}
+
+extension CGSize {
+    init(_ width:CGFloat, _ height:CGFloat) {
+        self.init(width:width, height:height)
+    }
+    func sizeByDelta(dw:CGFloat, dh:CGFloat) -> CGSize {
+        return CGSize(self.width + dw, self.height + dh)
+    }
+}
+extension CGPoint {
+    init(_ x:CGFloat, _ y:CGFloat) {
+        self.init(x:x, y:y)
+    } }
+extension CGVector {
+    init (_ dx:CGFloat, _ dy:CGFloat) {
+        self.init(dx:dx, dy:dy)
+    }
+}
+
+func dictionaryOfNames(_ arr:UIView...) -> [String:UIView] {
+    var d = [String:UIView]()
+    for (ix,v) in arr.enumerated() {
+        d["v\(ix+1)"] = v
+    }
+    return d
+}
+
+
+
+
+
+
+
+
+
+
+
