b797e0c57b8e44e52de64ade45d5393b98c9d62e
KeithPiTsui <85331524@qq.com>
2016-12-12 11:58:30 +0800

    before implement popup

---
+++ CryptoKB/KeyboardViewController+AutoSuffixPeriod.swift
@@ -0,0 +1,9 @@
+//
+//  KeyboardViewController+AutoSuffixPeriod.swift
+//  CryptoKeyboard
+//
+//  Created by Pi on 12/12/2016.
+//  Copyright © 2016 Keith. All rights reserved.
+//
+
+import Foundation
