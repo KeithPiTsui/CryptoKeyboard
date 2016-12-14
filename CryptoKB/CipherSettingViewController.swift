52b11fc454cd8ae4cdcba5c71f412763e46df460
KeithPiTsui <85331524@qq.com>
2016-12-14 13:33:33 +0800

    cipher selection panel

--- CryptoKB/CipherSettingViewController.swift
+++ CryptoKB/CipherSettingViewController.swift
@@ -12,6 +12,18 @@ protocol CipherSettingViewControllerDelegate: class {
     
 }
 
+extension UIView {
+    static func cipherSettingLineView(lineWidth width: CGFloat, lineColor color: UIColor) -> UIView {
+        let v = UIView()
+        v.translatesAutoresizingMaskIntoConstraints = false
+        v.widthAnchor.constraint(equalToConstant: width).isActive = true
+        v.backgroundColor = color
+        v.layer.cornerRadius = 3
+        v.clipsToBounds = true
+        return v
+    }
+}
+
 class CipherSettingViewController: UIViewController {
 
     weak var delegate: CipherSettingViewControllerDelegate?
@@ -39,6 +51,9 @@ class CipherSettingViewController: UIViewController {
         navigationItem.leftBarButtonItem = cancelBtn
         navigationItem.rightBarButtonItem = doneBtn
         navigationItem.titleView = cipherTopBar
+        
+        assembleElements()
+        layoutElements()
     }
     
     func cancel() {
@@ -50,6 +65,68 @@ class CipherSettingViewController: UIViewController {
         print("\(#function)")
         dismiss(animated: true, completion: nil)
     }
+    
+    private let morseLabel: UILabel = UILabel.keyboardLabel(font: KeyboardAppearanceScheme.keyboardViewItemInscriptFont, textColor: UIColor.keyboardViewItemInscriptColor, text: "Morse")
+    private let morseLine: UIView = UIView.cipherSettingLineView(lineWidth: 5, lineColor: UIColor.white)
+    private let caesarLabel: UILabel = UILabel.keyboardLabel(font: KeyboardAppearanceScheme.keyboardViewItemInscriptFont, textColor: UIColor.keyboardViewItemInscriptColor, text: "Caesar")
+    private let caesarLine: UIView = UIView.cipherSettingLineView(lineWidth: 5, lineColor: UIColor.white)
+    private let vigenereLabel: UILabel = UILabel.keyboardLabel(font: KeyboardAppearanceScheme.keyboardViewItemInscriptFont, textColor: UIColor.keyboardViewItemInscriptColor, text: "Vigenere")
+    private let vigenereLine: UIView = UIView.cipherSettingLineView(lineWidth: 5, lineColor: UIColor.white)
+    private let keywordLabel: UILabel = UILabel.keyboardLabel(font: KeyboardAppearanceScheme.keyboardViewItemInscriptFont, textColor: UIColor.keyboardViewItemInscriptColor, text: "Keyword")
+    private let keywordLine: UIView = UIView.cipherSettingLineView(lineWidth: 5, lineColor: UIColor.white)
+    private let cipherSelectSlider: UISlider = {let v = UISlider(); v.translatesAutoresizingMaskIntoConstraints = false; v.backgroundColor = UIColor.lightGray; return v}()
+    
+    private func assembleElements () {
+        view.addSubview(morseLabel)
+        view.addSubview(morseLine)
+        view.addSubview(caesarLabel)
+        view.addSubview(caesarLine)
+        view.addSubview(vigenereLabel)
+        view.addSubview(vigenereLine)
+        view.addSubview(keywordLabel)
+        view.addSubview(keywordLine)
+        view.addSubview(cipherSelectSlider)
+    }
+    
+    private func layoutElements() {
+        var constraints = [NSLayoutConstraint]()
+        constraints.append(NSLayoutConstraint(item: morseLabel, attribute: .centerX, relatedBy: .equal, toItem: view, attribute: .centerX, multiplier: 2*1/5, constant: 0))
+        constraints.append(NSLayoutConstraint(item: morseLabel, attribute: .bottom, relatedBy: .equal, toItem: caesarLabel, attribute: .top, multiplier: 1, constant: -2))
+        
+        constraints.append(NSLayoutConstraint(item: morseLine, attribute: .top, relatedBy: .equal, toItem: morseLabel, attribute: .bottom, multiplier: 1, constant: 0))
+        constraints.append(NSLayoutConstraint(item: morseLine, attribute: .centerX, relatedBy: .equal, toItem: morseLabel, attribute: .centerX, multiplier: 1, constant: 0))
+        constraints.append(NSLayoutConstraint(item: morseLine, attribute: .bottom, relatedBy: .equal, toItem: cipherSelectSlider, attribute: .top, multiplier: 1, constant: -2))
+        
+        constraints.append(NSLayoutConstraint(item: caesarLabel, attribute: .centerX, relatedBy: .equal, toItem: view, attribute: .centerX, multiplier: 2*2/5, constant: 0))
+        constraints.append(NSLayoutConstraint(item: caesarLabel, attribute: .centerY, relatedBy: .equal, toItem: view, attribute: .centerY, multiplier: 2*2/4, constant: 0))
+        
+        constraints.append(NSLayoutConstraint(item: caesarLine, attribute: .top, relatedBy: .equal, toItem: caesarLabel, attribute: .bottom, multiplier: 1, constant: 0))
+        constraints.append(NSLayoutConstraint(item: caesarLine, attribute: .centerX, relatedBy: .equal, toItem: caesarLabel, attribute: .centerX, multiplier: 1, constant: 0))
+        constraints.append(NSLayoutConstraint(item: caesarLine, attribute: .bottom, relatedBy: .equal, toItem: cipherSelectSlider, attribute: .top, multiplier: 1, constant: -2))
+        
+        constraints.append(NSLayoutConstraint(item: vigenereLabel, attribute: .centerX, relatedBy: .equal, toItem: view, attribute: .centerX, multiplier: 2*3/5, constant: 0))
+        constraints.append(NSLayoutConstraint(item: vigenereLabel, attribute: .bottom, relatedBy: .equal, toItem: keywordLabel, attribute: .top, multiplier: 1, constant: -2))
+        
+        constraints.append(NSLayoutConstraint(item: vigenereLine, attribute: .top, relatedBy: .equal, toItem: vigenereLabel, attribute: .bottom, multiplier: 1, constant: 0))
+        constraints.append(NSLayoutConstraint(item: vigenereLine, attribute: .centerX, relatedBy: .equal, toItem: vigenereLabel, attribute: .centerX, multiplier: 1, constant: 0))
+        constraints.append(NSLayoutConstraint(item: vigenereLine, attribute: .bottom, relatedBy: .equal, toItem: cipherSelectSlider, attribute: .top, multiplier: 1, constant: -2))
+        
+        constraints.append(NSLayoutConstraint(item: keywordLabel, attribute: .centerX, relatedBy: .equal, toItem: view, attribute: .centerX, multiplier: 2*4/5, constant: 0))
+        constraints.append(NSLayoutConstraint(item: keywordLabel, attribute: .centerY, relatedBy: .equal, toItem: view, attribute: .centerY, multiplier: 2*2/4, constant: 0))
+        
+        constraints.append(NSLayoutConstraint(item: keywordLine, attribute: .top, relatedBy: .equal, toItem: keywordLabel, attribute: .bottom, multiplier: 1, constant: 0))
+        constraints.append(NSLayoutConstraint(item: keywordLine, attribute: .centerX, relatedBy: .equal, toItem: keywordLabel, attribute: .centerX, multiplier: 1, constant: 0))
+        constraints.append(NSLayoutConstraint(item: keywordLine, attribute: .bottom, relatedBy: .equal, toItem: cipherSelectSlider, attribute: .top, multiplier: 1, constant: -2))
+        
+        constraints.append(NSLayoutConstraint(item: cipherSelectSlider, attribute: .centerX, relatedBy: .equal, toItem: view, attribute: .centerX, multiplier: 1, constant: 0))
+        constraints.append(NSLayoutConstraint(item: cipherSelectSlider, attribute: .centerY, relatedBy: .equal, toItem: view, attribute: .centerY, multiplier: 2*3/4, constant: 0))
+        constraints.append(NSLayoutConstraint(item: cipherSelectSlider, attribute: .left, relatedBy: .equal, toItem: view, attribute: .leftMargin, multiplier: 1, constant: 0))
+        constraints.append(NSLayoutConstraint(item: cipherSelectSlider, attribute: .right, relatedBy: .equal, toItem: view, attribute: .rightMargin, multiplier: 1, constant: 0))
+        
+        NSLayoutConstraint.activate(constraints)
+    }
+    
+    
 }
 
 
