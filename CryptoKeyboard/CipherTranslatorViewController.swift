afe99d50eba22250713d7e240d9835efc3bbd568
KeithPiTsui <85331524@qq.com>
2016-12-20 11:30:18 +0800

    refine translator

--- CryptoKeyboard/CipherTranslatorViewController.swift
+++ CryptoKeyboard/CipherTranslatorViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class CipherTranslatorViewController: UIViewController {
     
-    private var cipherType: CipherType = .morse
-    private var cipherKey: String = ""
+    private var cipherType: CipherType = .caesar
+    fileprivate var cipherKey: String = "03"
     private var cipherName: String { return CipherManager.ciphers[cipherType]!.name }
+    fileprivate var lastFocusTextView: Int = 0
+    
+    private var layoutConstraints: [NSLayoutConstraint] = []
+    private var keyAreaLayoutConstraints: [NSLayoutConstraint] = []
     
     private lazy var wholeArea: UIView = {
         let v = UIView()
@@ -29,13 +33,14 @@ class CipherTranslatorViewController: UIViewController {
         return v
     }()
     
-    private lazy var originalTextView: UITextView = {
+    fileprivate lazy var originalTextView: UITextView = {
         let tv = UITextView()
         tv.translatesAutoresizingMaskIntoConstraints = false
         tv.backgroundColor = UIColor.clear
         tv.text = "Hello"
         tv.textColor = UIColor(249,252,255)
         tv.font = UIFont.translatorOriginalTextFont
+        tv.delegate = self
         return tv
     }()
     
@@ -46,20 +51,21 @@ class CipherTranslatorViewController: UIViewController {
         return v
     }()
     
-    private lazy var translatedTextView: UITextView = {
+    fileprivate lazy var translatedTextView: UITextView = {
         let tv = UITextView()
         tv.translatesAutoresizingMaskIntoConstraints = false
         tv.backgroundColor = UIColor.clear
         tv.text = "Hola"
         tv.textColor = UIColor(43,181,255)
         tv.font = UIFont.translatorOriginalTextFont
+        tv.delegate = self
         return tv
     }()
     
     private lazy var tranlateBtn: UIButton = {
         let btn = UIButton(type: UIButtonType.roundedRect)
         btn.translatesAutoresizingMaskIntoConstraints = false
-        btn.setTitle("Trans", for: .normal)
+        btn.setTitle("⇅", for: .normal)
         btn.backgroundColor = UIColor.white
         btn.alpha = 0.8
         btn.layer.cornerRadius = 6
@@ -91,47 +97,62 @@ class CipherTranslatorViewController: UIViewController {
         originalArea.addSubview(originalTextView)
         wholeArea.addSubview(translatedArea)
         translatedArea.addSubview(translatedTextView)
-        
         wholeArea.addSubview(tranlateBtn)
-        
         wholeArea.addSubview(keyArea)
         
-        wholeArea.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor).isActive = true
-        wholeArea.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
-        wholeArea.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
-        wholeArea.bottomAnchor.constraint(equalTo: bottomLayoutGuide.topAnchor).isActive = true
+        assembleConstraints()
+        NSLayoutConstraint.activate(layoutConstraints)
+        NSLayoutConstraint.activate(keyAreaLayoutConstraints)
         
-        originalArea.leftAnchor.constraint(equalTo: wholeArea.leftAnchor).isActive = true
-        originalArea.rightAnchor.constraint(equalTo: wholeArea.rightAnchor).isActive = true
-        originalArea.topAnchor.constraint(equalTo: wholeArea.topAnchor).isActive = true
-        originalArea.heightAnchor.constraint(equalTo: wholeArea.heightAnchor, multiplier: 0.5).isActive = true
         
-        translatedArea.leftAnchor.constraint(equalTo: wholeArea.leftAnchor).isActive = true
-        translatedArea.rightAnchor.constraint(equalTo: wholeArea.rightAnchor).isActive = true
-        translatedArea.bottomAnchor.constraint(equalTo: wholeArea.bottomAnchor).isActive = true
-        translatedArea.topAnchor.constraint(equalTo: originalArea.bottomAnchor).isActive = true
-        
-        originalTextView.leftAnchor.constraint(equalTo: originalArea.leftAnchor, constant: 16).isActive = true
-        originalTextView.rightAnchor.constraint(equalTo: originalArea.rightAnchor, constant: -16).isActive = true
-        originalTextView.topAnchor.constraint(equalTo: originalArea.topAnchor, constant: 32).isActive = true
-        originalTextView.bottomAnchor.constraint(equalTo: originalArea.bottomAnchor, constant: -32).isActive = true
-        
-        translatedTextView.leftAnchor.constraint(equalTo: translatedArea.leftAnchor, constant: 16).isActive = true
-        translatedTextView.rightAnchor.constraint(equalTo: translatedArea.rightAnchor, constant: -16).isActive = true
-        translatedTextView.topAnchor.constraint(equalTo: translatedArea.topAnchor, constant: 8).isActive = true
-        translatedTextView.bottomAnchor.constraint(equalTo: translatedArea.bottomAnchor, constant: -8).isActive = true
-        
-        tranlateBtn.rightAnchor.constraint(equalTo: wholeArea.rightAnchor, constant:-12).isActive = true
-        tranlateBtn.centerYAnchor.constraint(equalTo: wholeArea.centerYAnchor).isActive = true
-        
-        keyArea.heightAnchor.constraint(equalToConstant: 30).isActive = true
-        keyArea.widthAnchor.constraint(equalTo: wholeArea.widthAnchor, multiplier: 0.4).isActive = true
-        keyArea.leftAnchor.constraint(equalTo: wholeArea.leftAnchor, constant:12).isActive = true
-        keyArea.centerYAnchor.constraint(equalTo: wholeArea.centerYAnchor).isActive = true
         
         // Do any additional setup after loading the view.
     }
+    
+    private func assembleConstraints() {
+        layoutConstraints.removeAll(keepingCapacity: true)
+        keyAreaLayoutConstraints.removeAll(keepingCapacity: true)
+        layoutConstraints.append(wholeArea.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor))
+        layoutConstraints.append(wholeArea.leftAnchor.constraint(equalTo: view.leftAnchor))
+        layoutConstraints.append(wholeArea.rightAnchor.constraint(equalTo: view.rightAnchor))
+        layoutConstraints.append(wholeArea.bottomAnchor.constraint(equalTo: bottomLayoutGuide.topAnchor))
+        
+        layoutConstraints.append(originalArea.leftAnchor.constraint(equalTo: wholeArea.leftAnchor))
+        layoutConstraints.append(originalArea.rightAnchor.constraint(equalTo: wholeArea.rightAnchor))
+        layoutConstraints.append(originalArea.topAnchor.constraint(equalTo: wholeArea.topAnchor))
+        layoutConstraints.append(originalArea.heightAnchor.constraint(equalTo: wholeArea.heightAnchor, multiplier: 0.5))
+        
+        layoutConstraints.append(translatedArea.leftAnchor.constraint(equalTo: wholeArea.leftAnchor))
+        layoutConstraints.append(translatedArea.rightAnchor.constraint(equalTo: wholeArea.rightAnchor))
+        layoutConstraints.append(translatedArea.bottomAnchor.constraint(equalTo: wholeArea.bottomAnchor))
+        layoutConstraints.append(translatedArea.topAnchor.constraint(equalTo: originalArea.bottomAnchor))
+        
+        layoutConstraints.append(originalTextView.leftAnchor.constraint(equalTo: originalArea.leftAnchor, constant: 16))
+        layoutConstraints.append(originalTextView.rightAnchor.constraint(equalTo: originalArea.rightAnchor, constant: -16))
+        layoutConstraints.append(originalTextView.topAnchor.constraint(equalTo: originalArea.topAnchor, constant: 32))
+        layoutConstraints.append(originalTextView.bottomAnchor.constraint(equalTo: originalArea.bottomAnchor, constant: -32))
+        
+        layoutConstraints.append(translatedTextView.leftAnchor.constraint(equalTo: translatedArea.leftAnchor, constant: 16))
+        layoutConstraints.append(translatedTextView.rightAnchor.constraint(equalTo: translatedArea.rightAnchor, constant: -16))
+        layoutConstraints.append(translatedTextView.topAnchor.constraint(equalTo: translatedArea.topAnchor, constant: 8))
+        layoutConstraints.append(translatedTextView.bottomAnchor.constraint(equalTo: translatedArea.bottomAnchor, constant: -8))
+        
+        layoutConstraints.append(tranlateBtn.rightAnchor.constraint(equalTo: wholeArea.rightAnchor, constant:-12))
+        layoutConstraints.append(tranlateBtn.centerYAnchor.constraint(equalTo: wholeArea.centerYAnchor))
+        
+        if cipherKey.chars.count > 0 {
+            keyAreaLayoutConstraints.append(keyArea.heightAnchor.constraint(equalToConstant: 30))
+            keyAreaLayoutConstraints.append(keyArea.widthAnchor.constraint(equalTo: wholeArea.widthAnchor, multiplier: 0.4))
+            keyAreaLayoutConstraints.append(keyArea.leftAnchor.constraint(equalTo: wholeArea.leftAnchor, constant:12))
+            keyAreaLayoutConstraints.append(keyArea.centerYAnchor.constraint(equalTo: wholeArea.centerYAnchor))
+        }
+    }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        translateMessage()
+    }
+    
     func tranlateButtonGetClick() {
         originalTextView.resignFirstResponder()
         translatedTextView.resignFirstResponder()
@@ -139,9 +160,15 @@ class CipherTranslatorViewController: UIViewController {
     }
     
     func translateMessage() {
-        guard let message = originalTextView.text else { return }
-        guard let translatedMsg = try? CipherManager.decrypt(message: message, withKey: cipherKey, andCipherType: cipherType) else { return }
-        translatedTextView.text = translatedMsg
+        if lastFocusTextView == 0 {
+            guard let message = originalTextView.text else { return }
+            guard let translatedMsg = try? CipherManager.encrypt(message: message, withKey: cipherKey, andCipherType: cipherType) else { return }
+            translatedTextView.text = translatedMsg
+        } else {
+            guard let message = translatedTextView.text else { return }
+            guard let translatedMsg = try? CipherManager.decrypt(message: message, withKey: cipherKey, andCipherType: cipherType) else { return }
+            originalTextView.text = translatedMsg
+        }
     }
     
     
@@ -184,10 +211,32 @@ class CipherTranslatorViewController: UIViewController {
 
 extension CipherTranslatorViewController: CipherSettingTopBarViewDelegate {
     func valuesForDisplay() -> [String] {
-        return ["1","2","3","4","5"]
+        return cipherKey.chars
     }
     func getTouched() {
         print("\(#function)")
+    }
+}
+
+extension CipherTranslatorViewController: UITextViewDelegate{
+    func textViewDidChange(_ textView: UITextView){
+        var tv = textView
+        if textView === originalTextView {
+            lastFocusTextView = 0
+            tv = translatedTextView
+        } else {
+            lastFocusTextView = 1
+            tv = originalTextView
+        }
+        translateMessage()
+        
+        
+        
+        let start = tv.text.index(before: tv.text.endIndex)
+        let startLocation = tv.text.distance(from: tv.text.startIndex, to: start)
+        let length = tv.text.distance(from: start, to: tv.text.endIndex)
+        tv.scrollRangeToVisible(NSMakeRange(startLocation, length))
+        
     }
 }
 
