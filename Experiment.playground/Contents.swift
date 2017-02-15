57167a23a42e9bba33d2f6798f32c9fa12ff83e7
KeithPiTsui <85331524@qq.com>
2017-02-15 08:23:12 +0800

    before extract keyboard related code into individual library

--- Experiment.playground/Contents.swift
+++ Experiment.playground/Contents.swift
@@ -4,6 +4,33 @@ import UIKit
 import Runes
 import KTKeyboard
 
+extension UIViewController {
+    public static var defaultNib: String {
+        return self.description().components(separatedBy: ".").dropFirst().joined(separator: ".")
+    }
+    
+    public static var storyboardIdentifier: String {
+        let x = self.description()
+        print(x)
+        let xs = x.components(separatedBy: ".")
+        print(xs)
+        let s = xs.dropFirst().joined(separator: ".")
+        print(s)
+        
+        return self.description().components(separatedBy: ".").dropFirst().joined(separator: ".")
+    }
+}
+
 
 let x = {$0+1} <^> [1,2,3]
 let kb = Keyboard.numberPunctuationKeyboardDiagram
+
+let u = UIViewController.storyboardIdentifier
+
+class KeyboardViewController: UIViewController {
+    
+}
+
+let x2 = KeyboardViewController.storyboardIdentifier
+
+let x9 = HelloWorld.greeting
---
+++ Experiment.playground/Sources/SourceInside.swift
@@ -0,0 +1,5 @@
+import Foundation
+
+public struct HelloWorld {
+    public static let greeting: String = "Hello world!"
+}
