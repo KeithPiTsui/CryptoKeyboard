96eea3124b5e1ed2090adfdae001cfb155a494a3
KeithPiTsui <85331524@qq.com>
2017-02-16 12:16:05 +0800

    before using storyboard to reconstruct UI

---
+++ CryptoKeyboard/AboutCryptoKeyboardViewModel.swift
@@ -0,0 +1,78 @@
+//
+//  AboutCrytoKeyboardViewModel.swift
+//  CryptoKeyboard
+//
+//  Created by Pi on 16/02/2017.
+//  Copyright © 2017 Keith. All rights reserved.
+//
+
+import UIKit
+import ReactiveCocoa
+import ReactiveSwift
+import Result
+
+/// Call by other
+public protocol AboutCryptoKeyboardViewModelInputs {
+    
+    /// Call when the view did load
+    func viewDidLoad()
+    
+    /// Call when the view will appear with animated property
+    func viewWillAppear(animated: Bool)
+    
+    /// Call when user tap on feedback row
+    func tapFeedback()
+}
+
+
+/// Emits Siganl outside
+public protocol AboutCryptoKeyboardViewModelOutputs {
+    var goToSendEmailFeedback: Signal<String, NoError> {get}
+}
+
+public protocol AboutCryptoKeyboardViewModelType {
+    var inputs: AboutCrytoKeyboardViewModelInputs {get}
+    var outputs: AboutCrytoKeyboardViewModelOutputs {get}
+}
+
+public final class AboutCryptoKeyboardViewModel: AboutCryptoKeyboardViewModelType, AboutCryptoKeyboardViewModelInputs, AboutCryptoKeyboardViewModelOutputs {
+    
+    public init() {
+        self.goToSendEmailFeedback = self.tapFeedbackProperty.signal.map({ return "Hello"})
+    }
+    
+    fileprivate let viewDidLoadProperty = MutableProperty()
+    public func viewDidLoad() {
+        self.viewDidLoadProperty.value = ()
+    }
+    
+    fileprivate let viewWillAppearProperty = MutableProperty<Bool?>(nil)
+    public func viewWillAppear(animated: Bool) {
+        self.viewWillAppearProperty.value = animated
+    }
+    
+    fileprivate let tapFeedbackProperty = MutableProperty()
+    public func tapFeedback() {
+        self.tapFeedbackProperty.value = ()
+    }
+    
+    public var goToSendEmailFeedback: Signal<String, NoError>
+    
+    
+    public var inputs: AboutCryptoKeyboardViewModelInputs {return self}
+    public var outputs: AboutCryptoKeyboardViewModelOutputs { return self}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ CryptoKeyboard/Storyboard.swift
@@ -0,0 +1,31 @@
+//
+//  Storyboard.swift
+//  CryptoKeyboard
+//
+//  Created by Pi on 16/02/2017.
+//  Copyright © 2017 Keith. All rights reserved.
+//
+
+import UIKit
+
+extension Bundle {
+    /// Returns an NSBundle pinned to the framework target. We could choose anything for the `forClass`
+    /// parameter as long as it is in the framework target.
+    internal static var framework: Bundle {
+        return Bundle(for: AboutCryptoKeyboardViewModel.self)
+    }
+}
+
+public enum Storyboard: String {
+    case AboutCryptoKeyboard
+    
+    
+    public func instantiate<VC: UIViewController>(_ viewController: VC.Type, inBundle bundle: Bundle = .framework) -> VC {
+        guard
+            let vc = UIStoryboard(name: self.rawValue, bundle: Bundle(identifier: bundle.identifier))
+                .instantiateViewController(withIdentifier: VC.storyboardIdentifier) as? VC
+            else { fatalError("Couldn't instantiate \(VC.storyboardIdentifier) from \(self.rawValue)") }
+        
+        return vc
+    }
+}
